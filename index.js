16a53d4543a3d498e08d66b3aa7e868eb3fa3f22
Matheus-daSilva <matheus2001silva@live.com>
2022-05-03 12:41:13 -0300

    add: app.post(/participants)

---
+++ index.js
@@ -0,0 +1,53 @@
+import cors from "cors";
+import express from "express";
+import joi from "joi";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+app.post("/participants", async (req, res) => {
+    const { name } = req.body;
+
+    const loginSchema = joi.object({
+        name: joi.string().required(),
+    });
+    const validation = loginSchema.validate({name});
+
+    if (validation.error) {
+        return res.sendStatus(422);
+    }
+
+    const loginName = {
+        name,
+        lastStatus: Date.now()
+    };
+    console.log(loginName)
+
+    try {
+        await mongoClient.connect();
+        const db = mongoClient.db("local");
+        const thereIs = await db.collection("user").findOne({ name });
+        console.log(thereIs)
+        if (thereIs) {
+            return res.sendStatus(409);
+        } else {
+            await db.collection("user").insertOne(loginName)
+            res.sendStatus(201);
+        }
+        mongoClient.close();
+    }
+    catch (e) {
+        console.log(e)
+        res.sendStatus(500);
+        mongoClient.close();
+    }
+
+})
+
+app.listen(5000);
